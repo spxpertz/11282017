06cfde93b5999b1cfbd48c246e577432e7e48765
farrukhqazi@evento.ae <Technip12@>
2017-11-28 21:50:23 +0500

    membership applied

--- SMS.MVC.Common/SMS.MVC.Web/Global.asax.cs
+++ SMS.MVC.Common/SMS.MVC.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebMatrix.WebData;
 
 namespace SMS.MVC.Web
 {
@@ -23,6 +24,8 @@ namespace SMS.MVC.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            //WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Users", "Id", "UserName", autoCreateTables: true);
         }
     }
 }
