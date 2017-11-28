420ee1ecd8b697c378458f0838c7b7d0e11affde
farrukhqazi@evento.ae <Technip12@>
2017-11-28 20:11:07 +0500

    project created

---
+++ SMS.MVC.Common/SMS.MVC.Common/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.MVC.Common
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SMS.MVC.Common/SMS.MVC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMS.MVC.Web.MvcApplication" Language="C#" %>
